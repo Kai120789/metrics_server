19bd817139a407d5535eed2044eef0be8b366614
Kai120789 <zoomer120789@gmail.com>
2024-10-24 22:15:59 +0300

    feat: add router

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 )
 
 require (
+	github.com/go-chi/chi/v5 v5.1.0
 	github.com/jackc/pgx/v4 v4.18.3
 	github.com/jackc/pgx/v5 v5.7.1
 	github.com/joho/godotenv v1.5.1
--- internal/transport/http/router/router.go
+++ internal/transport/http/router/router.go
@@ -1,1 +1,31 @@
 package router
+
+import (
+	"net/http"
+	"server/internal/transport/http/handler"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type Router interface {
+	SetUpdates(w http.ResponseWriter, r *http.Request)
+	SetUpdate(w http.ResponseWriter, r *http.Request)
+	SetMetric(w http.ResponseWriter, r *http.Request)
+	GetMetricValue(w http.ResponseWriter, r *http.Request)
+	GetGetHTML(w http.ResponseWriter, r *http.Request)
+}
+
+func New(h *handler.Handler) http.Handler {
+	r := chi.NewRouter()
+
+	r.Route("api/", func(r chi.Router) {
+		r.Post("updates", h.SetUpdates)
+		r.Post("update", h.SetUpdate)
+		r.Post("/{type}/{name}/{value}", h.SetMetric)
+		r.Get("/value/{type}/{name}", h.GetMetricValue)
+	})
+
+	r.Get("/", h.GetHTML)
+
+	return r
+}
