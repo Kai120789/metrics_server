82b267ad95a23e937d54018803c28bf1427746ca
Kai120789 <zoomer120789@gmail.com>
2024-10-26 14:50:01 +0300

    chore: add migrator

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS metrics (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL,
+    name VARCHAR(50),
     type VARCHAR(50),
     value FLOAT,
     delta INTEGER,
