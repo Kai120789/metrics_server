279aa55bfd383e61329c6e2a7b79bd979ddaaa0f
Kai120789 <zoomer120789@gmail.com>
2024-11-04 12:42:57 +0300

    feat: tests for handler

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.1
 require go.uber.org/zap v1.27.0
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -14,9 +15,12 @@ require (
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
@@ -24,5 +28,6 @@ require (
 	github.com/jackc/pgx/v4 v4.18.3
 	github.com/jackc/pgx/v5 v5.7.1
 	github.com/joho/godotenv v1.5.1
+	github.com/stretchr/testify v1.9.0
 	go.uber.org/multierr v1.10.0 // indirect
 )
---
+++ internal/transport/http/handler/handler_test.go
@@ -0,0 +1,160 @@
+package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"server/internal/config"
+	"server/internal/dto"
+	"server/internal/models"
+	"server/internal/utils"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+)
+
+type MockHandlerer struct {
+	mock.Mock
+}
+
+func (m *MockHandlerer) SetUpdates(metrics []dto.Metric) ([]models.Metric, error) {
+	args := m.Called(metrics)
+	return args.Get(0).([]models.Metric), args.Error(1)
+}
+
+func (m *MockHandlerer) SetMetric(metric dto.Metric) (*models.Metric, error) {
+	args := m.Called(metric)
+	return args.Get(0).(*models.Metric), args.Error(1)
+}
+
+func (m *MockHandlerer) GetMetricValue(name, typeStr string) (*int64, error) {
+	args := m.Called(name, typeStr)
+	return args.Get(0).(*int64), args.Error(1)
+}
+
+func (m *MockHandlerer) GetHTML(w http.ResponseWriter) error {
+	args := m.Called(w)
+	return args.Error(0)
+}
+
+func TestSetUpdates(t *testing.T) {
+	// init mock for handler and injections
+	mockService := new(MockHandlerer)
+	logger := zap.NewNop()
+	conf := &config.Config{SecretKey: "test-secret"}
+	h := New(mockService, logger, conf)
+
+	// metrics for send and res
+	metrics := []dto.Metric{
+		{Name: "metric1", Type: "gauge", Value: new(float64)},
+		{Name: "metric2", Type: "counter", Delta: new(int64)},
+	}
+	reqBody, _ := json.Marshal(metrics)
+
+	// expected result
+	expectedMetrics := []models.Metric{}
+
+	mockService.On("SetUpdates", mock.MatchedBy(func(arg []dto.Metric) bool {
+		// check is value match
+		return len(arg) == len(metrics) &&
+			arg[0].Name == metrics[0].Name &&
+			arg[0].Type == metrics[0].Type &&
+			arg[1].Name == metrics[1].Name &&
+			arg[1].Type == metrics[1].Type
+	})).Return(expectedMetrics, nil)
+
+	// gen hash
+	expectedHash := utils.GenerateHash(conf.SecretKey)
+	req := httptest.NewRequest(http.MethodPost, "/updates", bytes.NewBuffer(reqBody))
+	req.Header.Set("Hash", expectedHash) // compare hash
+	w := httptest.NewRecorder()
+
+	h.SetUpdates(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	// check status code
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+	mockService.AssertExpectations(t)
+}
+
+func TestSetMetric(t *testing.T) {
+	// init mock for handler and injections
+	mockService := new(MockHandlerer)
+	logger := zap.NewNop()
+	conf := &config.Config{}
+	h := New(mockService, logger, conf)
+
+	// init router
+	r := chi.NewRouter()
+	r.Post("/{type}/{name}/{value}", h.SetMetric)
+
+	value := 10.0
+	metric := dto.Metric{
+		Name:  "test_metric",
+		Type:  "gauge",
+		Value: &value,
+	}
+	mockService.On("SetMetric", metric).Return(&models.Metric{}, nil)
+
+	req := httptest.NewRequest(http.MethodPost, "/gauge/test_metric/10", nil)
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+	mockService.AssertCalled(t, "SetMetric", metric)
+}
+
+func TestGetMetricValue(t *testing.T) {
+	mockService := new(MockHandlerer)
+	logger := zap.NewNop()
+	conf := &config.Config{}
+	h := New(mockService, logger, conf)
+
+	r := chi.NewRouter()
+	r.Get("/value/{type}/{name}", h.GetMetricValue)
+
+	value := int64(100)
+	mockService.On("GetMetricValue", "test_metric", "gauge").Return(&value, nil)
+
+	req := httptest.NewRequest(http.MethodGet, "/value/gauge/test_metric", nil)
+	w := httptest.NewRecorder()
+
+	r.ServeHTTP(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusCreated, resp.StatusCode)
+	mockService.AssertCalled(t, "GetMetricValue", "test_metric", "gauge")
+}
+
+func TestGetHTML(t *testing.T) {
+	mockService := new(MockHandlerer)
+	logger := zap.NewNop()
+	conf := &config.Config{}
+	h := New(mockService, logger, conf)
+
+	mockService.On("GetHTML", mock.Anything).Return(nil)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+
+	h.GetHTML(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	mockService.AssertCalled(t, "GetHTML", mock.Anything)
+}
